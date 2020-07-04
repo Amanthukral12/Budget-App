054062c9dde81c70f1d4aef253cfb9abcd61dfc1
Aman Thukral <thukral618@gmail.com>
2020-07-04 22:47:00 +0530

    commit on 'master'

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>this is budget app</p>, document.getElementById('app'));
+
