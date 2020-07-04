259cdc69ec848520efe603a9fd38dc3b3f41603b
Aman Thukral <thukral618@gmail.com>
2020-07-05 02:47:40 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>this is budget app</p>, document.getElementById('app'));
+
+const ExpenseDashboardPage = () => (
+    <div>
+        this is from my dashboard
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        this is from my add expense component
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        this is from my edit expense component
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        this is from my help component
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+        </div>
+
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
 
