ddec79674b10a0aa475a16edbfad65e2150024fa
Aman Thukral <thukral618@gmail.com>
2020-07-06 21:51:12 +0530

    commit

---
+++ src/playground/destructuring.js
@@ -0,0 +1,13 @@
+const person = {
+    name: 'Aman',
+    age: 21,
+    location: {
+        city: 'Delhi',
+        temp: 42
+    }
+};
+
+const { name, age } = person;
+
+
+console.log(`${name} is ${age}.`);
