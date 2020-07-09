a8fc3cae1da035203945cea2f4decd4146eb9dff
Aman Thukral <thukral618@gmail.com>
2020-07-09 22:28:18 +0530

    commit

--- src/playground/redux-budgetapp.js
+++ src/playground/redux-budgetapp.js
@@ -1,4 +1,64 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid'
+
+const addExpense = (
+    {
+        description = '',
+        note = '',
+        amount = 0,
+        createdAt = 0
+    } = {}
+) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
+
+
+
+
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+const filterReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+
+const filtersReducer = (state = filterReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+
+//console.log(store.getState());
 
 const demoState = {
     expenses: [{
@@ -6,7 +66,7 @@ const demoState = {
         description: 'hello',
         note: 'hello hi bye',
         amount: 100,
-        createAt: 0
+        createdAt: 0
     }],
     filters: {
         text: 'rent',
