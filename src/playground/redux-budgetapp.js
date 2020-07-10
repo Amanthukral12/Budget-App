1b2e74fbf6c4b03591be723a452e1e0a949b873f
Aman Thukral <thukral618@gmail.com>
2020-07-10 18:25:06 +0530

    refactoring

--- src/playground/redux-budgetapp.js
+++ src/playground/redux-budgetapp.js
@@ -30,28 +30,6 @@ const editExpense = (id, updates) => ({
     updates
 });
 
-const setTextFilter = (text = '') => ({
-    type: 'SET_TEXT_FILTER',
-    text
-});
-
-const sortByDate = () => ({
-    type: 'SORT_BY_DATE'
-});
-
-const sortByAmount = () => ({
-    type: 'SORT_BY_AMOUNT'
-});
-
-const setStartDate = (startDate = '') => ({
-    type: 'SET_START_DATE',
-    startDate
-});
-
-const setEndDate = (endDate = '') => ({
-    type: 'SET_END_DATE',
-    endDate
-});
 
 
 const expensesReducerDefaultState = [];
