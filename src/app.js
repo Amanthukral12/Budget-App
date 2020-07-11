c02ec174af26087c25ce442e40b15884df9a943e
Aman Thukral <thukral618@gmail.com>
2020-07-12 03:49:55 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -19,6 +20,8 @@ const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
 console.log(visibleExpenses);
 
-
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+    <AppRouter />
+);
+ReactDOM.render(jsx, document.getElementById('app'));
 
---
+++ src/playground/hoc.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Info = (props) => (
+    <div>
+        <h1>Info</h1>
+        <p>The info is is: {props.info}</p>
+    </div>
+);
+
+const withAdminWarning = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAdmin && <p>Warning: private info</p>}
+            <WrappedComponent {...props} />
+        </div>
+    );
+};
+
+const requireAuthentication = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAuthenticated ? (<WrappedComponent {...props} />) : (<p>this require authentication</p>)}
+
+        </div>
+    );
+};
+
+
+// const AdminInfo = withAdminWarning(Info);
+
+const AuthInfo = requireAuthentication(Info);
+
+// ReactDOM.render(<AdminInfo isAdmin={true} info="There are the details" />, document.getElementById('app'));
+ReactDOM.render(<AuthInfo isAuthenticated={false} info="There are the details" />, document.getElementById('app'));
