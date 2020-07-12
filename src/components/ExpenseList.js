7b6dff3150a338f138a9a63457b9b33dbc5d2e6a
Aman Thukral <thukral618@gmail.com>
2020-07-13 00:36:41 +0530

    commit

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import ExpenseListItem from './ExpenseListItem';
+import selectedExpenses from '../selectors/expenses';
 
 const ExpenseList = (props) => (
     <div>
         <h1>Expense List</h1>
-        {props.filters.text}
-        {props.expenses.length}
+        {props.expenses.map((expense) => {
+            return <ExpenseListItem key={expense.id} {...expense} />;
+        })}
     </div>
 );
 
 const mapStateToProps = (state) => {
     return {
-        expenses: state.expenses,
-        filters: state.filters
+        expenses: selectedExpenses(state.expenses, state.filters)
     };
 };
 
