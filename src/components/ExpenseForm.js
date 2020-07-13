87993698de721c86ab654ed6656df8802e224828
Aman Thukral <thukral618@gmail.com>
2020-07-14 01:08:50 +0530

    commit

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default class ExpenseForm extends React.Component {
+    state{
+    description: ''
+};
+onDescriptionChange =
+    render() {
+    return (
+        <div>
+            <form>
+                <input
+                    type="text"
+                    placeholder="Description"
+                    autoFocus
+                    value={this.state.description}
+                    onChange={this.onDescriptionChange}
+                />
+                <input
+                    type="number"
+                    placeholder="Amount"
+                />
+                <textarea
+                    placeholder="Add a note for your expense (optional)"
+                >
+                </textarea>
+                <button>Add Expense</button>
+            </form>
+        </div>
+    )
+}
+}
