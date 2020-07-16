539d856c3ad8aa0cbc86e0db95a4dd39b44f597a
Aman Thukral <thukral618@gmail.com>
2020-07-16 23:41:50 +0530

    commit

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class ExpenseForm extends React.Component {
     state{
-    description: ''
+    description: '',
 };
 onDescriptionChange =
     render() {
