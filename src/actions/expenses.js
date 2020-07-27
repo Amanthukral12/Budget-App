85f6f0576b0081229eea5aec4d2e8b6766e2f501
Aman Thukral <thukral618@gmail.com>
2020-07-27 06:20:27 +0530

    updating

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -3,61 +3,69 @@ import database from '../firebase/firebase';
 
 // ADD_EXPENSE
 export const addExpense = (expense) => ({
-  type: 'ADD_EXPENSE',
-  expense
+    type: 'ADD_EXPENSE',
+    expense
 });
 
 export const startAddExpense = (expenseData = {}) => {
-  return (dispatch) => {
-    const {
-      description = '',
-      note = '',
-      amount = 0,
-      createdAt = 0
-    } = expenseData;
-    const expense = { description, note, amount, createdAt };
+    return (dispatch) => {
+        const {
+            description = '',
+            note = '',
+            amount = 0,
+            createdAt = 0
+        } = expenseData;
+        const expense = { description, note, amount, createdAt };
 
-    return database.ref('expenses').push(expense).then((ref) => {
-      dispatch(addExpense({
-        id: ref.key,
-        ...expense
-      }));
-    });
-  };
+        return database.ref('expenses').push(expense).then((ref) => {
+            dispatch(addExpense({
+                id: ref.key,
+                ...expense
+            }));
+        });
+    };
 };
 
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
-  type: 'REMOVE_EXPENSE',
-  id
+    type: 'REMOVE_EXPENSE',
+    id
 });
 
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
-  type: 'EDIT_EXPENSE',
-  id,
-  updates
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
 });
 
 // SET_EXPENSES
 export const setExpenses = (expenses) => ({
-  type: 'SET_EXPENSES',
-  expenses
+    type: 'SET_EXPENSES',
+    expenses
 });
 
 export const startSetExpenses = () => {
-  return (dispatch) => {
-    return database.ref('expenses').once('value').then((snapshot) => {
-      const expenses = [];
+    return (dispatch) => {
+        return database.ref('expenses').once('value').then((snapshot) => {
+            const expenses = [];
 
-      snapshot.forEach((childSnapshot) => {
-        expenses.push({
-          id: childSnapshot.key,
-          ...childSnapshot.val()
+            snapshot.forEach((childSnapshot) => {
+                expenses.push({
+                    id: childSnapshot.key,
+                    ...childSnapshot.val()
+                });
+            });
+
+            dispatch(setExpenses(expenses));
         });
-      });
+    };
+};
 
-      dispatch(setExpenses(expenses));
-    });
-  };
+export const startRemoveExpenses = ({ id } = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({ id }));
+        });
+    };
 };
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,9 @@ const webpack = require('webpack');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'test') {
+    require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
     require('dotenv').config({ path: '.env.development' });
 }
 
