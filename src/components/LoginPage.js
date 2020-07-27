37df46517ed7f54887c318cf9831e3ef7ae26a2b
Aman Thukral <thukral618@gmail.com>
2020-07-28 04:34:18 +0530

    adding Styling

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
-    <div>
-        <button onClick={startLogin}>Login</button>
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">Budget App</h1>
+            <p>It's time to get your expenses under control.</p>
+            <button onClick={startLogin}>Login</button>
+        </div>
+
     </div>
 );
 
