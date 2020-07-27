6a648f7c99f19221584628a889f9f8ee68154a1d
Aman Thukral <thukral618@gmail.com>
2020-07-28 03:20:06 +0530

    adding PublicRoute

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { startLogout } from '../actions/auth';
 const Header = ({ startLogout }) => (
   <header>
     <h1>Budget-App</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to="/dashboard" activeClassName="is-active" >Dashboard</NavLink>
     <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
     <button onClick={startLogout}>Logout</button>
   </header>
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,11 +4,12 @@ import createHistory from 'history/createBrowserHistory';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage';
+
 import NotFoundPage from '../components/NotFoundPage';
 
 import LoginPage from '../components/LoginPage';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
@@ -18,11 +19,11 @@ const AppRouter = () => (
     <div>
 
       <Switch>
-        <Route path="/" component={LoginPage} exact={true} />
+        <PublicRoute path="/" component={LoginPage} exact={true} />
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
         <PrivateRoute path="/create" component={AddExpensePage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
+
         <Route component={NotFoundPage} />
       </Switch>
     </div>
