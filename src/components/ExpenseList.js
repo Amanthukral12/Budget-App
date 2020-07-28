ba230d57e4162d197f35854b4991bbbbaf0a1fa4
Aman Thukral <thukral618@gmail.com>
2020-07-29 00:01:03 +0530

    styling

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,16 +4,25 @@ import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
 export const ExpenseList = (props) => (
-  <div>
-    {
-      props.expenses.length === 0 ? (
-        <p>No expenses</p>
-      ) : (
-          props.expenses.map((expense) => {
-            return <ExpenseListItem key={expense.id} {...expense} />;
-          })
-        )
-    }
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-wide">Expense</div>
+      <div className="show-for-wide">Amount</div>
+    </div>
+    <div className="list-body">
+      {
+        props.expenses.length === 0 ? (
+          <div className="list-item list-item--message">
+            <span>No Expenses</span>
+          </div>
+        ) : (
+            props.expenses.map((expense) => {
+              return <ExpenseListItem key={expense.id} {...expense} />;
+            })
+          )
+      }
+    </div>
   </div>
 );
 
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -4,16 +4,18 @@ import moment from 'moment';
 import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
-  <div>
-    <Link to={`/edit/${id}`}>
-      <h3>{description}</h3>
-    </Link>
-    <p>
-      {numeral(amount / 100).format('0,0')}
-      -
-      {moment(createdAt).format('MMMM Do, YYYY')}
-    </p>
-  </div>
+  <Link className="list-item" to={`/edit/${id}`}>
+    <div>
+      <h3 className="list-item__title">{description}</h3>
+      <span className="list-item__sub-title">
+        {moment(createdAt).format('MMMM Do, YYYY')}
+      </span>
+    </div>
+    <h3 className="list-item__data">
+      ₹{numeral(amount / 100).format('0,0')}
+    </h3>
+  </Link>
+
 );
 
 export default ExpenseListItem;
